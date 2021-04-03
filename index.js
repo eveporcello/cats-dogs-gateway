a091bd59033017d75e30edc03c2024ae4e9649bd
eveporcello <eve@moonhighway.com>
2021-04-03 15:02:02 -0700

    Adds new routes

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const { ApolloGateway } = require("@apollo/gateway");
 
 const gateway = new ApolloGateway({
   serviceList: [
-    { name: "cats", url: "http://localhost:4001" },
-    { name: "dogs", url: "http://localhost:4002" }
+    {
+      name: "cats",
+      url: "https://cats-federation.herokuapp.com"
+    },
+    {
+      name: "dogs",
+      url: "https://dogs-federation.herokuapp.com/"
+    }
   ]
 });
 
