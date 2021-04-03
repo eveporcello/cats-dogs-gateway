75b5d03f24f1e7ee33144dacee0408a11d2ea9d4
eveporcello <eve@moonhighway.com>
2021-04-03 16:40:47 -0700

    Adds gateway

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const gateway = new ApolloGateway();
 
 const PORT = process.env.PORT || 4000;
 
-(async () => {
-  const { schema, executor } = await gateway.load();
+const server = new ApolloServer({
+  gateway,
+  subscriptions: false
+});
 
-  const server = new ApolloServer({ schema, executor });
-
-  server.listen({ port: PORT }).then(({ port }) => {
-    console.log(`🐩 Pet Gateway available at ${port}`);
-  });
-})();
+server.listen({ port: PORT }).then(({ port }) => {
+  console.log(`🐩 Pet Gateway available at ${port}`);
+});
