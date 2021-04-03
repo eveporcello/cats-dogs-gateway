4625f4680cffce627f7acc6a15ddcdd28bfb80cb
eveporcello <eve@moonhighway.com>
2021-04-03 15:57:28 -0700

    gateway

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 const { ApolloServer } = require("apollo-server");
 const { ApolloGateway } = require("@apollo/gateway");
 
-const gateway = new ApolloGateway({
-  serviceList: [
-    {
-      name: "cats",
-      url: "https://cats-federation.herokuapp.com"
-    },
-    {
-      name: "dogs",
-      url: "https://dogs-federation.herokuapp.com/"
-    }
-  ]
-});
+const gateway = new ApolloGateway();
 
 const PORT = process.env.PORT || 4000;
 
