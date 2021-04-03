f61eeef9f9f2f6d53450c301fefefa292f2bd1cf
eveporcello <eve@moonhighway.com>
2021-04-03 15:02:21 -0700

    ports and friends

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ const gateway = new ApolloGateway({
   ]
 });
 
+const PORT = process.env.PORT || 4000;
+
 (async () => {
   const { schema, executor } = await gateway.load();
 
   const server = new ApolloServer({ schema, executor });
 
-  server.listen(4000).then(({ url }) => {
-    console.log(`🐩 Pet Gateway available at ${url}`);
+  server.listen({ port: PORT }).then(({ port }) => {
+    console.log(`🐩 Pet Gateway available at ${port}`);
   });
 })();
