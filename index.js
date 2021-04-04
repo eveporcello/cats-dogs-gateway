6e2f6508b8b10a171bf8b2f292197105d6533da3
eveporcello <eve@moonhighway.com>
2021-04-03 17:51:07 -0700

    Adds start files

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const { ApolloServer } = require("apollo-server");
 const { ApolloGateway } = require("@apollo/gateway");
 
-const gateway = new ApolloGateway({ debug: true });
+const gateway = new ApolloGateway({
+  serviceList: [
+    {
+      name: "cats",
+      url: "https://cats-federation.herokuapp.com"
+    },
+    {
+      name: "dogs",
+      url: "https://dogs-federation.herokuapp.com/"
+    }
+  ],
+  debug: true
+});
 
 const PORT = process.env.PORT || 4000;
 
